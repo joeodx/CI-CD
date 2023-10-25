220a2583414ed0fad510770b7e2c2b678c69a711
142804583+alexjbrowntech@users.noreply.github.com <142804583+alexjbrowntech@users.noreply.github.com>
2023-10-25 12:27:59 +0100

    MVC structure set up

--- app.js
+++ app.js
@@ -4,10 +4,4 @@ const PORT = 4000;
 
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.send("Welcome to conversation starters API");
-});
-
-app.listen(PORT, function () {
-  console.log(`Server is now listening on http://localhost:${PORT}`);
-});
+import { icebreakerRoutes } from "./routes/icebreakerRoutes.js";
---
+++ routes/icebreakerRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+export const icebreakerRoutes = express.Router();
+
+icebreaker.get("/conversation_starter", function (req, res) {
+  const starter_Text = await;
+});
