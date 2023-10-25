ceb8adcdb4e607bb582502ebee60420597f529d3
Rikiah <rik_28@hotmail.co.uk>
2023-10-25 14:27:38 +0100

    Plan added

--- controllers/icebreakerController.js
+++ controllers/icebreakerController.js
@@ -1,1 +1,15 @@
 //import
+import { promises as fs } from "node:fs";
+import path from "node:path";
+
+const filePath = path.resolve(process.cwd(), "convo_starters.json");
+
+export async function getIcebreaker() {}
+
+// Declare json file to a variable
+
+// Function to generate random number between 1 - 20 (Stretch goal: make number relative to number of responses)
+
+// Take random number and extract key value pair from json
+
+// Store and Return the value
--- routes/icebreakerRoutes.js
+++ routes/icebreakerRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
+import * as icebreaker from "../controllers/icebreakerController.js";
 
 export const icebreakerRoutes = express.Router();
 
-icebreaker.get("/conversation_starter", function (req, res) {
-  const starter_Text = await;
-});
+icebreaker.get("/conversation_starter", function (req, res) {});
